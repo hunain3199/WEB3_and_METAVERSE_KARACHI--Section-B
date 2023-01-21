b7eaf48324b1979ba84222f6d6c8ca0f447e67dc
hunain3199 <muhammadhunain3199@gmail.com>
2023-01-21 17:39:51 +0500

    npm i word_counterr

---
+++ Typescript_Node-Projects/project05_word_counter/index.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const answers = await inquirer.prompt([{
+        name: 'sentence',
+        type: 'input',
+        message: "Enter your sentence to count the word = "
+    }]);
+const words = answers.sentence.trim().split(" ");
+console.log(chalk.bgGreenBright(`Your sentence word count is ${words.length}.`));
--- Typescript_Node-Projects/project05_word_counter/index.ts
+++ Typescript_Node-Projects/project05_word_counter/index.ts
@@ -2,3 +2,28 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
 import chalkAnimation from "chalk-animation"
+
+const sleep = () => {
+    return new Promise((res) => {
+        setTimeout(res,2000);
+    })
+}
+
+async function welcome(){
+    let rainbow = chalkAnimation.rainbow('Lets start Your word counter game')
+    await sleep();
+    rainbow.stop()
+}
+await welcome()
+
+
+const answers : {
+    sentence:string
+} = await inquirer.prompt([{
+    name:'sentence',
+    type:'input',
+    message:"Enter your sentence to count the word = "
+}])
+
+const words = answers.sentence.trim().split(" ")
+console.log(chalk.bgGreenBright(`Your sentence word count is ${words.length}.`))
